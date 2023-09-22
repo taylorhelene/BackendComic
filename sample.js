c7e87a3a3090eebf8afa6677c095f5bd9170e042
lesmadeit <lesmwendwa@gmail.com>
2023-09-22 15:26:59 +0300

    two working arguments changes

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 var sqlite3 = require('sqlite3');
 const { gql } = require('apollo-server');
 
-let db= new sqlite3.Database('./mu.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+let db= new sqlite3.Database('./dl.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err && err.code == "SQLITE_CANTOPEN") {
         createDatabase();
         return;
@@ -13,7 +13,7 @@ let db= new sqlite3.Database('./mu.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CR
 });
 
 function createDatabase() {
-    var newdb = new sqlite3.Database('mu.db', (err) => {
+    var newdb = new sqlite3.Database('dl.db', (err) => {
         if (err) {
             console.log("Getting error " + err);
             exit(1);
@@ -29,39 +29,51 @@ function createTables(newdb) {
         hero_name text not null,
         is_xman text not null,
         was_snapped text not null,
-        image text not null
+        image text not null,
+        views int not null
     );
-   
-    insert into hero (hero_id, hero_name, is_xman, was_snapped, image)
-        values (1, 'Spiderman', 'N', 'Y', 'https://www.transparentpng.com/thumb/spiderman/ld3cDH-countdown-launch-marvelu-spider-man-playstation.png'),
-               (2, 'Tony Stark', 'N', 'N', 'https://www.transparentpng.com/thumb/spiderman/ld3cDH-countdown-launch-marvelu-spider-man-playstation.png'),
-               (3, 'Jean Grey', 'Y', 'N', 'https://www.transparentpng.com/thumb/spiderman/ld3cDH-countdown-launch-marvelu-spider-man-playstation.png');
+        `, ()  => {
+            secondQuery(newdb);
+    });
+}
 
+function secondQuery(newdb){
+    newdb.exec(`
+    insert into hero (hero_id, hero_name, is_xman, was_snapped, image , views)
+    values (1, 'Spiderman', 'N', 'Y', 'https://www.transparentpng.com/thumb/spiderman/ld3cDH-countdown-launch-marvelu-spider-man-playstation.png', 0),
+           (2, 'Tony Stark', 'N', 'N', 'https://www.transparentpng.com/thumb/iron-man/b4NpBS-iron-man-clipart-png-file.png', 0),
+           (3, 'Jean Grey', 'Y', 'N', 'https://www.transparentpng.com/thumb/phoenix/1zlrBH-phoenix-picture.png', 0);
+    `,()=>{
+        thirdQuery(newdb);
+    })
+}
+
+function thirdQuery(newdb){
+    newdb.exec(`
     create table hero_power (
         hero_id int not null,
-        hero_power text not null
-    );
+        hero_power text not null);
+    `,()=>{
+        fourthQuery(newdb);
+    })
+}
 
-    insert into hero_power (hero_id, hero_power)
+function fourthQuery(newdb){
+    newdb.exec(
+     `insert into hero_power (hero_id, hero_power)
         values (1, 'Web Slinging'),
                (1, 'Super Strength'),
                (1, 'Total Nerd'),
                (2, 'Total Nerd'),
                (3, 'Telepathic Manipulation'),
                (3, 'Astral Projection');
-        `, ()  => {
-            runQueries(newdb);
-    });
+               ` ,()=>{
+                runQueries(newdb);
+               });
 }
 
 function runQueries(db) {
-    db.all(`select hero_name, is_xman, was_snapped from hero h
-   inner join hero_power hp on h.hero_id = hp.hero_id
-   where hero_power = ?`, "Total Nerd", (err, rows) => {
-        rows.forEach(row => {
-            console.log(row.hero_name + "\t" +row.is_xman + "\t" +row.was_snapped);
-        });
-    });
+  
 
     db.serialize(function () {
         db.all("WITH tables AS (SELECT name tableName, sql FROM sqlite_master WHERE type = 'table' AND tableName NOT LIKE 'sqlite_%') SELECT fields.name, fields.type, tableName FROM tables CROSS JOIN pragma_table_info(tables.tableName) fields", function (err, tables) {
@@ -70,7 +82,7 @@ function runQueries(db) {
     });
 
     db.serialize(()=>{
-        const statemnt=db.prepare("select * from hero")
+        const statemnt=db.prepare("select * from hero_power")
         statemnt.all((err,tables)=>{
             console.log(tables);
             console.log(err);
@@ -88,6 +100,7 @@ type Hero{
     snapped: String
     power :String
     image: String
+    views:Int
 }
 
 type Power{
@@ -101,6 +114,10 @@ type Query{
 
 }
 
+type Mutation{
+    hero(views:Int! , id:ID!): Hero
+}
+
 `
 
 const resolvers = {
@@ -112,14 +129,14 @@ const resolvers = {
            
             function getRecords(){
                 return new Promise((resolve,reject)=>{
-                db.prepare("SELECT  h.hero_id id ,hero_name name,is_xman xman,was_snapped snapped, hero_power power, image image FROM hero  h join hero_power hp on h.hero_id = hp.hero_id ").all((err,rows)=>{
+                db.prepare("SELECT  h.hero_id id ,hero_name name,is_xman xman,was_snapped snapped, hero_power power, image image ,views views FROM hero  h join hero_power hp on h.hero_id = hp.hero_id ").all((err,rows)=>{
                   if(err){
                       return console.error(err.message);
                   }
                   rows.forEach((row)=>{
                       myans.push(row);
                   });
-                  
+                console.log(myans)
                  resolve(myans);
               })
                 
@@ -147,12 +164,36 @@ const resolvers = {
                     })
                 })
             }
-            record = getHero();;
+            record = getHero();
             return record;
             
 
            
            
+        }
+    },
+    Mutation : {
+
+        hero:(_,{views,id})=>{
+            var myans= {}
+            var record={}
+
+            function changeHero(){
+                return new Promise((resolve,reject)=>{
+                    db.prepare("UPDATE hero SET views= ? WHERE hero_id= ? ").run([views,id],(err,rows)=>{
+                        if(err){
+                            return console.error(err.message);
+                        }
+                        myans=rows;
+                        console.log(rows)
+                        return(myans=rows,resolve(myans))
+
+                    })
+                })
+            }
+
+            record=changeHero();
+            return record
         }
     }
 }
